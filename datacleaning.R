9c1de2a6606f164d3cd279556bc0facb58a141ce
devanshpahuja <65828718+devanshpahuja@users.noreply.github.com>
2020-08-28 01:01:48 +0530

    Update datacleaning.R

--- datacleaning.R
+++ datacleaning.R
@@ -71,3 +71,52 @@ train$Outlet_Size <- predict(modelForest, newdata = train)
 nRows <- nrow(train)
 nCompRows <- sum(complete.cases(train))
 nCompRows/nRows
+
+summary(train)
+
+#REPEATING FOR TEST MODEL, To Predict
+
+test$Item_Fat_Content <- gsub("LF", "lowfat",test$Item_Fat_Content)
+test$Item_Fat_Content <- gsub("low fat", "lowfat",test$Item_Fat_Content)
+test$Item_Fat_Content <- gsub("Low Fat", "lowfat",test$Item_Fat_Content)
+test$Item_Fat_Content <- gsub("reg", "Regular",test$Item_Fat_Content)
+test$Item_Fat_Content <- as.factor(test$Item_Fat_Content)
+MeanItem_Weight <- mean(test$Item_Weight[!is.na(test$Item_Weight)])
+test$Item_Weight[is.na(test$Item_Weight)] <- MeanItem_Weight
+test_temp <- test %>% filter(test$Item_Visibility != 0)
+visibility_model <- lm(test$Item_Visibility ~ test$Item_Weight + test$Item_Fat_Content +
+                         test$Item_Type + test$Item_MRP +
+                         test$Outlet_Establishment_Year + test$Outlet_Size +
+                         test$Outlet_Location_Type,
+                       data = test_temp)
+test$Item_Visibility[test$Item_Visibility == 0] <-
+  predict(visibility_model,newdata = test[test$Item_Visibility == 0,])
+set.seed(100)
+test$Outlet_Size <- as.character(test$Outlet_Size)
+Storetypes <- subset(test, Outlet_Size != "")
+spl <- sample.split(Storetypes$Outlet_Size, SplitRatio = 0.8283468)
+test_outlet <- subset(Storetypes, spl == TRUE)
+test_outlet <- subset(Storetypes, spl == FALSE)
+test_outlet$Outlet_Size <- as.factor(test_outlet$Outlet_Size)
+test_outlet$Outlet_Size <- as.factor(test_outlet$Outlet_Size)
+SizeForest <- randomForest(Outlet_Size ~.-Item_Identifier,
+                           data =  test_outlet,nodesize = 25, ntree = 100)  
+PredictForest <- predict(SizeForest, newdata = test_outlet)
+test$Outlet_Size <- predict(SizeForest, newdata = test)
+
+
+#Build your test and sales linear model
+testPrediction <- test
+linearsalemodel <- lm(Item_Outlet_Sales ~ Item_Weight + Item_Fat_Content +
+                    Item_Visibility + Item_MRP + 
+                    Outlet_Establishment_Year + Outlet_Size +
+                    Outlet_Location_Type + Outlet_Type,
+                  data = train)
+sales <- predict(linearsalemodel, newdata = testPrediction)
+testPrediction$Item_Outlet_Sales <- as.vector(sales)
+
+# Rename the predicted sales column
+names(testPrediction)[12] <- "Item_Outlet_Sales"
+
+answer = testPrediction[, c("Item_Identifier", "Outlet_Identifier", "Item_Outlet_Sales")]
+write.csv(answer, "result.csv")
